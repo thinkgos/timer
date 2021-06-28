3512a4903ed52578379c81668d5816bc9aeca1ad
thinkgo <thinkgo@aliyun.com>
2021-06-28 11:22:03 +0800

    增加延迟队列

---
+++ delayqueue/delay_queue.go
@@ -0,0 +1,74 @@
+package delayqueue
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/things-go/container/priorityqueue"
+)
+
+type Delayed interface {
+	DelayMs() int64
+}
+
+type DelayQueue struct {
+	mu     sync.Mutex
+	pq     *priorityqueue.Queue
+	signal chan struct{}
+}
+
+func NewDelayQueue() *DelayQueue {
+	return &DelayQueue{
+		pq:     priorityqueue.New(),
+		signal: make(chan struct{}, 1),
+	}
+}
+
+func (sf *DelayQueue) Add(d Delayed) {
+	sf.mu.Lock()
+	sf.pq.Add(d)
+	sf.mu.Unlock()
+	select {
+	case sf.signal <- struct{}{}:
+	default:
+	}
+}
+
+func (sf *DelayQueue) Pop(ctx context.Context) Delayed {
+	for {
+		sf.mu.Lock()
+		e := sf.pq.Peek()
+		if e == nil {
+			sf.mu.Unlock()
+
+			select {
+			case <-sf.signal:
+				continue
+			case <-ctx.Done():
+				return nil
+			}
+		}
+
+		first := e.(Delayed)
+		delay := first.DelayMs()
+		if delay <= 0 {
+			sf.pq.Poll()
+			sf.mu.Unlock()
+			return first
+		}
+		sf.mu.Unlock()
+
+		tm := time.NewTimer(time.Duration(delay) * time.Millisecond)
+		select {
+		case <-sf.signal:
+			tm.Stop()
+			continue
+		case <-ctx.Done():
+			tm.Stop()
+			return nil
+		case <-tm.C:
+			continue
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/things-go/timer
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/stretchr/testify v1.7.0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/things-go/container v0.0.0-20210628010040-e8f44b210915
 )
