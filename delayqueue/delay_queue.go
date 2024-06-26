feceb7ac7f59c6bf297543736cdb1e366627c429
thinkgos <thinkgo@aliyun.com>
2024-06-26 16:37:42 +0800

    fix: improve delay queue perf

--- delayqueue/delay_queue.go
+++ delayqueue/delay_queue.go
@@ -2,6 +2,7 @@ package delayqueue
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/things-go/timer/queue"
@@ -16,7 +17,7 @@ type DelayQueue[T Delayed] struct {
 	mu            sync.Mutex
 	priorityQueue *queue.PriorityQueue[T]
 	notify        chan struct{}
-	waiting       bool
+	waiting       atomic.Bool
 	phantom       T
 }
 
@@ -28,19 +29,12 @@ func NewDelayQueue[T Delayed]() *DelayQueue[T] {
 }
 
 func (dq *DelayQueue[T]) Add(val T) {
-	var wakeUp bool
-
 	dq.mu.Lock()
 	dq.priorityQueue.Add(val)
-	if dq.waiting {
-		first, exist := dq.priorityQueue.Peek()
-		if exist && first.CompareTo(val) == 0 {
-			wakeUp = true
-			dq.waiting = false
-		}
-	}
+	first, exist := dq.priorityQueue.Peek()
+	wakeup := exist && first.CompareTo(val) == 0 && dq.waiting.CompareAndSwap(true, false)
 	dq.mu.Unlock()
-	if wakeUp {
+	if wakeup {
 		select {
 		case dq.notify <- struct{}{}:
 		default:
@@ -51,9 +45,9 @@ func (dq *DelayQueue[T]) Add(val T) {
 func (dq *DelayQueue[T]) Take(quit <-chan struct{}) (t T, exit bool) {
 	for {
 		dq.mu.Lock()
-		first, exist := dq.priorityQueue.Peek()
+		head, exist := dq.priorityQueue.Peek()
 		if !exist {
-			dq.waiting = true
+			dq.waiting.Store(true)
 			dq.mu.Unlock()
 
 			select {
@@ -62,24 +56,25 @@ func (dq *DelayQueue[T]) Take(quit <-chan struct{}) (t T, exit bool) {
 			case <-quit:
 				return dq.phantom, true
 			}
-		}
-
-		delay := first.DelayMs()
-		if delay <= 0 {
-			dq.priorityQueue.Poll()
+		} else {
+			delay := head.DelayMs()
+			if delay <= 0 {
+				dq.priorityQueue.Poll()
+				dq.mu.Unlock()
+				return head, false
+			}
+			dq.waiting.Store(true)
 			dq.mu.Unlock()
-			return first, false
-		}
-		dq.waiting = true
-		dq.mu.Unlock()
-		tm := time.NewTimer(time.Duration(delay) * time.Millisecond)
-		select {
-		case <-quit:
-			tm.Stop()
-			return dq.phantom, true
-		case <-dq.notify:
-			tm.Stop()
-		case <-tm.C:
+			tm := time.NewTimer(time.Duration(delay) * time.Millisecond)
+			select {
+			case <-dq.notify:
+				tm.Stop()
+			case <-quit:
+				tm.Stop()
+				return dq.phantom, true
+			case <-tm.C:
+				dq.waiting.Store(false)
+			}
 		}
 	}
 }
