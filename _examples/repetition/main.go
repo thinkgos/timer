918c09d3ad74d1c45b805b100da1d7e422d198b0
thinkgos <thinkgo@aliyun.com>
2024-08-16 08:54:28 +0800

    ci: exaple repetition

--- _examples/repetition/main.go
+++ _examples/repetition/main.go
@@ -7,14 +7,16 @@ import (
 	"github.com/thinkgos/timer/timed"
 )
 
+// one or two second delay repetition example
 func main() {
+	i := 0
 	task := timed.NewTask(time.Second)
 	task.WithJobFunc(func() {
 		now := time.Now().String()
-		timed.AddTask(task)
+		i++
+		timed.AddTask(task.SetDelay(time.Second * time.Duration((i%2 + 1))))
 		fmt.Printf("%s: repetition executed,\n", now)
 	})
-
 	timed.AddTask(task)
 	select {}
 }
